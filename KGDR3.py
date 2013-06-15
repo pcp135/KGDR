528967ae0c498d736afb59d3cc2576fc1c0db31a
Phil <pcp135@gmail.com>
2013-06-14 23:13:12 -0400

    added code to help review for best model paramaters

--- KGDR3.py
+++ KGDR3.py
@@ -3,11 +3,9 @@ from sklearn import svm, metrics
 from sklearn.cross_validation import KFold
 from scipy import stats
 
-X = np.genfromtxt('train.csv', delimiter=',')
-test_set = np.genfromtxt('test.csv', delimiter=',')
+X = np.genfromtxt('reducedtrain.csv', delimiter=',')
 Y = X[:,0]
 X = X[:,1:]/255
-test_set /= 255
 print "Data sets loaded and split"
 
 kf = KFold(len(Y), n_folds=5, indices=False)
@@ -16,7 +14,6 @@ for model_num, (train, test) in enumerate(kf):
 
 	X_train, X_test, Y_train, Y_test = X[train], X[test], Y[train], Y[test]
 
-	# Create a classifier: a support vector classifier
 	classifier[model_num] = svm.SVC(C=100,cache_size=750,gamma=0.01)
 
 	# We learn the digits on the first half of the digits
@@ -28,19 +25,7 @@ for model_num, (train, test) in enumerate(kf):
 
 	print "Classification report for classifier on cross validation set %s:\n%s\n" % (
 	    classifier[model_num], metrics.classification_report(expected, predicted))
+	print "Overall accuracy with C = %f and gamma = %f: %f" % (
+			C_val, gamma_val, metrics.accuracy_score(expected, predicted))
 	#print "Confusion matrix:\n%s" % metrics.confusion_matrix(expected, predicted)
 
-pred=np.zeros(test_set.shape[0])
-#val=np.zeros(X.shape[0])
-for i, model in enumerate(classifier):
-	#val=np.vstack((val, model.predict(X)))
-	pred=np.vstack((pred, model.predict(test_set)))
-
-#voted = stats.mode(val[1:6],axis=0)[0][0]
-#print metrics.classification_report(Y, voted)
-voted = stats.mode(pred[1:6],axis=0)[0][0]
-
-f=open('result5.csv','w')
-for value in voted:
-	f.write('%s\n' % (int(value),))
-f.close()
