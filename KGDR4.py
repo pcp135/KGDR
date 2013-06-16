872bdca838c428897d952ac482f49ffb9a836aa1
Phil <pcp135@gmail.com>
2013-06-16 00:46:52 -0400

    updated rf to do better grid search

--- KGDR4.py
+++ KGDR4.py
@@ -11,24 +11,29 @@ with open('KGDR4.log','a') as f:
 	X = X[:,1:]/255
 	f.write("\n\nStarting at %s\n" % dt.now())
 	f.write("Data sets loaded and split\n")
-
-	kf = KFold(len(Y), n_folds=5, indices=False)
-	classifier = [0]*5
-	accuracy = [0]*5
-	for model_num, (train, test) in enumerate(kf):
-
-		X_train, X_test, Y_train, Y_test = X[train], X[test], Y[train], Y[test]
-
-		classifier[model_num] = RandomForestClassifier(n_estimators=50,max_features=30)
-
-		classifier[model_num].fit(X_train, Y_train)
-
-		expected = Y_test
-		predicted = classifier[model_num].predict(X_test)
-		accuracy[model_num] = metrics.accuracy_score(expected, predicted)
-
-		f.write ("Accuracy on fold %i: %f\n" % (model_num+1, accuracy[model_num]))
 	
-	f.write ("%s\n" % classifier[model_num])
-	f.write ("Overall accuracy = %f\n" % (np.mean(accuracy),))
+	for ests in [10,30,60,100,300]:
+		for feats in [10,20,28,30,40]:
+			kf = KFold(len(Y), n_folds=5, indices=False)
+			classifier = [0]*5
+			accuracy = [0]*5
+			for model_num, (train, test) in enumerate(kf):
+
+				X_train, X_test, Y_train, Y_test = X[train], X[test], Y[train], Y[test]
+
+				classifier[model_num] = RandomForestClassifier(
+					n_estimators=ests,max_features=feats)
+
+				classifier[model_num].fit(X_train, Y_train)
+
+				expected = Y_test
+				predicted = classifier[model_num].predict(X_test)
+				accuracy[model_num] = metrics.accuracy_score(expected, predicted)
+
+				f.write ("Accuracy on fold %i with estimators = %i, features = %i: %f\n" % (
+					model_num+1, ests, feats, accuracy[model_num]))
+	
+			f.write ("%s\n" % classifier[model_num])
+			f.write ("Overall accuracy with estimators = %i, features = %i: %f\n" % (
+				ests, feats, np.mean(accuracy)))
 	f.write("Finishing at %s\n" % dt.now())
